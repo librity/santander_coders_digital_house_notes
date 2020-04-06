1c90f0d64005384bacd51d8009bd80cba037ced0
Luis Geniole <luis.geniole@gmail.com>
2020-04-06 02:16:52 -0300

    Add Internal Server error template sample.

--- class8/pizza_express/src/config/youch.js
+++ class8/pizza_express/src/config/youch.js
@@ -1,28 +1,25 @@
 import Youch from 'youch';
 
-export default (err, req, res, next) => {
+export default async (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
-    const errors = new Youch(err, req);
-
-    return errors
+    const errors = await new Youch(err, req)
       .addLink(({ message }) => {
         const url = `https://stackoverflow.com/search?q=${encodeURIComponent(
           `[express] ${message}`
         )}`;
+
         return `<a href="${url}" target="_blank" title="Search on stackoverflow"><i class="fab fa-stack-overflow"></i></a>`;
       })
       .addLink(({ message }) => {
         const url = `https://www.google.com/search?q=${encodeURIComponent(
-          `[express] ${message}`
+          `express ${message}`
         )}`;
+
         return `<a href="${url}" target="_blank" title="Search on google"><i class="fab fa-google"></i></a>`;
       })
-      .toHTML()
-      .then((html) => {
-        res.writeHead(500, { 'content-type': 'text/html' });
-        res.write(html);
-        res.end();
-      });
+      .toHTML();
+
+    return res.status(500).send(errors).end();
   }
 
   return res.status(500).json({ error: 'Internal server error.' });
